bedafb455edea87b49299c43093184304841b1ee
akash <akash@arivani.net>
2024-05-01 11:22:47 +0530

    PUSH11

--- my_tennis_club/my_tennis_club/settings.py
+++ my_tennis_club/my_tennis_club/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-h&$m7c-icjqv8pg^78-d#wshe3o9xq!k-#96b9!73t7d1)o+#6'
+SECRET_KEY = 'django-insecure-=ta*i2s5#)yj9(8-x+$e1#_8wrk8=qh%rh7q8(jy74znk=lv8+'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
